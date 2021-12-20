0ced294dcaf00d220d2ca28669aeb4f995c3321e
M <mikhail.k.q@yandex.ru>
2021-12-20 17:19:19 +0300

    update

--- tests.py
+++ tests.py
@@ -1,14 +1,11 @@
 import unittest
 from unittest.mock import patch
 import saper
-import pygame
 
 class SaperTest(unittest.TestCase):
-
-    @patch('pygame.key.get_pressed')
-    def setup(self, test_patch):
-        test_patch.return_value = 'win_test'
-        self.assertEqual(saper.lose(), 'win_test')
+    def test1(self):
+        tile1 = saper.tile(1,1)
+        self.assertEqual(tile1, saper.gettile(1,1))
 
 
 if __name__ == "__main__":
