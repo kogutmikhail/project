22cdef2f24b25bb8fd3e5ce619fae2983f5b46cb
M <mikhail.k.q@yandex.ru>
2021-12-20 17:45:10 +0300

    update

--- tests.py
+++ tests.py
@@ -1,10 +1,13 @@
 import unittest
-from unittest.mock import patch
 import saper
+from pygame.locals import *
 
 class SaperTest(unittest.TestCase):
     def test1(self):
-        tile1 = saper.tile(1,1)
+        tile1 = saper.tile(Rect(10, 10, 17, 17))
+        tile1.x = 1
+        tile1.y = 1
+        saper.tiles = [tile1]
         self.assertEqual(tile1, saper.gettile(1,1))
 
 
